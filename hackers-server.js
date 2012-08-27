d0649b0f3fee3f0905fe25111780e787b682e88f
robinouu <robinouu@gmail.com>
2012-08-27 14:54:57 +0200

    hackers now uses classes (see description)
    Client / Server : for connection purpose
    Command / CommandList : an API to register and execute commands in the
    CLI
    Utils : some convenience functions

--- hackers-server.js
+++ hackers-server.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /*
  * hackers-server.js
  *
@@ -7,129 +9,87 @@
  * Licensed under the new BSD license.
  */
 
-var util = require("util"), color = require('colors');
-var fs = require("fs"), readline = require("readline"), net = require('net');
-var rl = readline.createInterface(process.stdin, process.stdout);
-var wormhole = require("wormhole");
-var program = require("commander");
+var util = require('util'),
+	color = require("colors"),
+	program = require("commander"),	
+	
+	Server = require("./lib/Server.js"),
+	CommandList = require("./lib/CommandList.js"),
+	Command = require("./lib/Command.js");
 
+/* Program variables */
 program
-  .version('0.0.1')
-  .option('-p, --port <n>', 'The port to listen for sockets connections', parseInt)
-  .parse(process.argv);
+	.version('0.0.1')
+	.option('-p, --port <port>', 'The port to listen for sockets connections.', parseInt)
+	.option('-r, --retryInterval <milliseconds>', 'The number of milliseconds before restarting connexion when an error occurs.', parseInt)
+	.parse(process.argv);
   
 if (typeof program.port !== 'number' || program.port < 0)
 	program.port = 8081;
+if (typeof program.retryInterval !== 'number' || program.retryInterval < 0)
+	program.retryInterval = 2000;	
+
+/* Create the server */
+var server = new Server();
+
+server.on('clientConnected', function(client)
+{
+	util.log('New client connected (' + client.stream.remoteAddress + ')');
 	
-function Client(stream, username)
-{
-	this.username = (typeof username === 'string') ? username : '';
-	this.stream = stream;
-}
-
-Array.prototype.remove = function(e)
-{
-	for (var i = 0; i < this.length; i++)
+	/* Write in the hall channel of the client a welcome message */
+	client.stream.write('hall',
 	{
-		if (e == this[i])
-			return this.splice(i, 1);
-	}
-};
-
-var clients = [];
-
-var server = net.createServer({ allowHalfOpen: true}, function (stream)
-{
-	stream.setEncoding("utf8");
-
-	wormhole(stream, 'hall', function (data)
-	{
-        if (data.id === 'login' && typeof data.username === 'string')
-		{
-           stream.write('hall', {id : 'login-success', msg : 'You are now logged on the server !'});
-        }
-    });
-	
-	stream.on('connect', function()
-	{
-		var client = new Client(stream);
-		clients.push(client);
-
-		util.log('New client connected (' + stream.remoteAddress + ')');
-		
-		stream.write('hall',
-		{
-			id : 'welcome',
-			msg : "Welcome to the " + "HACKERS SERVER".cyan.bold + " (" + program.version() + ")\n\nType " + "[help]".bold.green + " for available commands"
-		});
+		id : 'welcome',
+		msg : "Welcome to the " + "HACKERS SERVER".cyan.bold + " (" + program.version() + ")\n\nType " + "[help]".bold.green + " for available commands"
 	});
-
 });
 
-util.log("Type " + "[start]".bold.green + " to launch the server");
+server.on('started', function()
+{
+	util.log("Server started on port " + program.port.toString().bold.green);
+	cmdList.prompt();
+});
 
+server.on('stopped', function()
+{
+	util.log("Server stopped.");
+	cmdList.prompt();
+});
+
+/* Create a CLI */
+var cmdList = new CommandList();
+var cmdStart = new Command("start", /^start$/, function ()
+{
+	server.start(program.port);
+});
+
+var cmdStop = new Command("stop", /^stop$/, function ()
+{
+	server.stop();
+});
+
+/* Register commands */
+cmdList.register(cmdStart);
+cmdList.register(cmdStop);
+
+/* Process error handler */
 process.on('uncaughtException', function(e)
 {
+	/* if port is already in use, we retry every X milliseconds */
 	if (e.code == 'EADDRINUSE')
 	{	
 		setTimeout(function ()
 		{
 			console.log('Address in use, retrying...');
-			serverStart();
-		}, 2000);
-	}
-});
-
-var serverStarted = false;
-var serverStart = function()
-{
-	server.listen(program.port, function()
-	{
-		if (!serverStarted) // hack for listening
-		{
-			serverStarted = true;
-			util.log(("Server started on port ".bold) + program.port.toString().bold.green);
-		}
-	});
-}
-
-rl.on('line', function (line)
-{
-	cmd = line.toLowerCase();
-	
-	if (cmd === 'start')
-	{
-		serverStart();
-	}
-	else if (cmd === 'users')
-	{
-		clients.forEach(function(c)
-		{
-			util.log("- " + c.name + "\n");
-		});
-	}
-	else if (cmd === 'stop')
-	{
-		try
-		{
-			server.close();
-			util.log("Server stopped.".bold);
-		}
-		catch (e)
-		{
-			util.debug(e.toString().red);
-		}
+			server.start(program.port);
+		}, program.retryInterval);
 	}
 	else
 	{
-		util.debug("Unknow command for the server.".red);
+		util.debug(e);
+		cmdList.prompt();
 	}
-
-	
-}).on('close', function()
-{
-	util.log("Program is exiting...".bold);
-	
-	process.exit(0);
-	process.stdin.destroy();
 });
+
+
+cmdList.prompt();
